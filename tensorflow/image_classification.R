43b9c22b51cf25730b2be876e43e7136f2c3e9a5
jinhuizi-fu <fujinhuizi@gmail.com>
2021-03-30 20:00:56 -0400

    image classifier practice with tensorflow

---
+++ tensorflow/image_classification.R
@@ -0,0 +1,10 @@
+
+library(keras)
+
+# data inputs
+fashion_mnist <- dataset_fashion_mnist()
+
+c(train_images, train_labels) %<-% fashion_mnist$train
+c(test_images, test_labels) %<-% fashion_mnist$test
+
+
